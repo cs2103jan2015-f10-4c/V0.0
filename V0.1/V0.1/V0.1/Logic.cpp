28b4c6b4526ed96587e1eaaf788e68040e501601
jiaaayu <jia_yu_1993@hotmail.com>
2015-03-10 22:59:33 +0800

    updated

--- V0.1/V0.1/V0.1/Logic.cpp
+++ V0.1/V0.1/V0.1/Logic.cpp
@@ -1,39 +1,59 @@
-#include "deleteExecutor.h"
+#include "Logic.h"
 
-deleteExecutor::deleteExecutor(void){
+Logic::addTask(string taskName, string taskDate, string startTime, string duration, Storage& storage, Display& display) {
+    taskList = storage.getTaskStorage();
+    Task tempStorage;
+    tempStorage.taskDescription = taskName;
+    tempStorage.date = taskDate;
+    tempStorage.startingTime = startTime;
+    tempStorage.duration = duration;
+    taskList.push_back(tempStorage);
 }
 
-void deleteExecutor::deleteTask(size_t index, Display& display, Storage& storage) {
-	taskList = storage.getTaskStorage();
-	if (index > taskList.size()) {
-		display::displayError();
-	}
+Logic::updateStorage(vector<Task> taskList, Storage& storage) {
+    storage.updateFile (taskList);
+}
 
-	else {
-		string deletedText;
+Logic::deleteTask(size_t index, Display& display, Storage& storage) {
+    taskList = storage.getTaskStorage();
+    if (index > taskList.size()) {
+        display::displayError();
+    }
+    
+    else {
+        string deletedText;
 		deletedText = taskList[index - 1];
 		taskList.erase(taskList.begin() + (index - 1));
-	}
-	display.displayAll(vector<Task> taskList);
+    }
+    display.displayAll(vector<Task> taskList);
 }
 
-void deleteExecutor::updateStorage(vector<Task> taskList, Storage& storage) {
-	storage.updateFile(taskList);
+Logic::editTaskTitle(int index, string newName){
+    taskList = storage.getTaskList();
+    taskList[index+1].taskName = newName;
 }
 
-
-#include <vector>
-#include 'addExecutor.h'
-
-addExecutor::addExecutor(void){
+Logic::editTaskDate(int index, string newDate){
+    taskList = storage.getTaskList();
+    taskList[index+1].taskDate = newDate;
 }
 
-void addExecutor::addTask(string taskName, string taskDate, string startTime, string duration, Storage& storage, Display& display) {
-	taskList = storage.getTaskStorage();
-	Task tempStorage;
-	tempStorage.taskDescription = taskName;
-	tempStorage.date = taskDate;
-	tempStorage.startingTime = startTime;
-	tempStorage.duration = duration;
-	taskList.push_back(tempStorage);
+Logic::editTaskStartingTime(int index, string newStartingTime){
+    taskList = storage.getTaskList();
+    taskList[index+1].taskName = newStartingTime;
+}
 
+Logic::editTaskEndingTime(int index, string newEndingTime){
+    taskList = storage.getTaskList();
+    taskList[index+1].taskName = newEndingTime;
+}
+
+Logic::searchTask(string keyPhrase, Display& display){
+    taskList = storage.getTaskList();
+    for (int i=0; i < taskList.size(); i++) {
+        if (taskList[i].taskName.compare(keyPhrase) == 0) {
+            searchResult.push_back(taskList[i]);
+        }
+    }
+    display.displayRequired(searchResult);
+}
--- V0.1/V0.1/V0.1/Logic.h
+++ V0.1/V0.1/V0.1/Logic.h
@@ -25,12 +25,20 @@ private:
 public:
 	Logic();
 	~Logic();
-	void addExecutor(string, string, string);
+	/*void addExecutor(string, string, string);
 	void deleteExecutor(int);
 	void searchExecutor(string);
 	void editExecutor(int, string, string, string);
 	void displayResult(vector<Task>);
-
+    */
+    void addTask(string, string, string, string, vector<Task>&, Storage&, Display&);
+    void updateStorage(vector<Task>&, Storage&);
+    void deleteTask(size_t index, Display&, Storage&);
+    void editTaskTitle(int, string);
+    void editTaskDate(int, string);
+    void editTaskStartTime(int, string);
+    void editTaskDuration(int, string);
+    void searchTask(string, Display&);
 };
 
 
