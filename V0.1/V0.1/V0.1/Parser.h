84953c100584e194f86b800acf9b44ed937bdd67
README <ChiMing710@8-91.priv23.nus.edu.sg>
2015-03-12 14:18:10 +0800

    no message

--- V0.1/V0.1/V0.1/Parser.h
+++ V0.1/V0.1/V0.1/Parser.h
@@ -1,5 +1,4 @@
 
-
 #ifndef PARSER_H
 #define PARSER_H
 
@@ -14,31 +13,38 @@
 using namespace std;
 
 class Parser{
-
+    
 private:
-	Logic logic;
-	static enum COMMAND_TYPE 
-	{COMMAND_ADD,  COMMAND_DELETE, COMMAND_SEARCH, COMMAND_EDIT};
-
-	static  string USER_PROMPT_TASKNAME ;
-	static  string USER_PROMPT_STARTTIME;
-	static  string USER_PROMPT_ENDTIME;
-	static  string USER_PROMPT_INDEX;
-	static  string USER_PROMPT_KEYWORD ;
-	static  string USER_PROMPT_NEWTASKNAME ;
-	static  string USER_PROMPT_NEWSTARTTIME ;
-	static  string USER_PROMPT_NEWENDTIME ;
-
+    Logic logic;
+    static enum COMMAND_TYPE {COMMAND_ADD,  COMMAND_DELETE, COMMAND_SEARCH, COMMAND_EDIT};
+    static const string DELIMITERS;
+    static const string TIME_INDICATORS[];
+    static const int NO_OF_TIME_INDICATORS;
+    
+    static  string USER_PROMPT_TASKNAME ;
+    static  string USER_PROMPT_STARTTIME;
+    static  string USER_PROMPT_ENDTIME;
+    static  string USER_PROMPT_INDEX;
+    static  string USER_PROMPT_KEYWORD ;
+    static  string USER_PROMPT_NEWTASKNAME ;
+    static  string USER_PROMPT_NEWSTARTTIME ;
+    static  string USER_PROMPT_NEWENDTIME ;
+    
+    string getCommandType(string);
+    string getUserTask(string);
+    string getStartDetail(string);
+    string getEndDetail(string);
+    
 public:
-
-	Parser();
-	~Parser();
-
-	bool determineCommandType(string);
-
-	void processCommand(COMMAND_TYPE);
-
-
+    
+    Parser();
+    ~Parser();
+    
+    bool determineCommand(string);
+    
+    void processCommand(COMMAND_TYPE);
+    
+    
 };
 
 #endif
