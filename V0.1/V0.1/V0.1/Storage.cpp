3e30d6d6402a785b35d67ad508580888b130a1a6
LI HAOYI <jason19920910@gmail.com>
2015-03-12 16:55:52 +0800

    add different types of task in storage

--- V0.1/V0.1/V0.1/Storage.cpp
+++ V0.1/V0.1/V0.1/Storage.cpp
@@ -13,28 +13,52 @@ const string TEXTFILENAME = "rushhour.txt";
 Storage::Storage(void){}
 Storage::~Storage(void){}
 
-void Storage::storeTask(string task, string startingTime, string endingTime){
+/*void Storage::storeTask(string task, string startingTime, string endingTime){
 	Task tempStorage;
 	tempStorage.taskName = task;
 	tempStorage.startingTime = startingTime;
 	tempStorage.endingTime = endingTime;
 	//tempStorage.date = date;
 	taskList.push_back(tempStorage);
-};
+};*/
 
 void Storage::updateTaskList(vector<Task> taskStorage){
 	taskList = taskStorage;
 };
 
+void Storage::updateDeadlineTasklist(vector<Task> deadlineTaskStorage){
+	deadlineTaskList = deadlineTaskStorage;
+};
+
+void Storage::updateFloatingTasklist(vector<Task> floatingTaskStorage){
+	floatingTaskList = floatingTaskStorage;
+};
+
 void Storage::saveFile(string TEXTFILENAME){
 	ofstream writeFile;
 	writeFile.open(TEXTFILENAME.c_str());
+	
+	writeFile << " Normal Tasklist:" << endl;
 	for (int i = 0; i < taskList.size(); i++){
 		writeFile << i + 1 << ".";
 		writeFile << taskList[i].taskName << "from";
 		//writeFile << taskList[i].date;
 		writeFile << taskList[i].startingTime << "to";
 		writeFile << taskList[i].endingTime << endl;
+	}
+	
+	writeFile << " Deadline Tasklist:" << endl;
+	for (int i = 0; i < deadlineTaskList.size(); i++){
+		writeFile << i + 1 << ".";
+		writeFile << deadlineTaskList[i].taskName << "at";
+		//writeFile << taskList[i].date;
+		writeFile << deadlineTaskList[i].startingTime << endl;
+	}
+
+	writeFile << " Floating Tasklist:" << endl;
+	for (int i = 0; i < floatingTaskList.size(); i++){
+		writeFile << i + 1 << ".";
+		writeFile << floatingTaskList[i].taskName << endl;
 	}
 	writeFile.close();
 };
@@ -43,3 +67,10 @@ vector<Task> Storage::getTaskList(){
 	return taskList;
 };
 
+vector<Task> Storage::getDeadlineTaskList(){
+	return deadlineTaskList;
+};
+
+vector<Task> Storage::getFloatingTasklist(){
+	return floatingTaskList;
+};
--- V0.1/V0.1/V0.1/Storage.h
+++ V0.1/V0.1/V0.1/Storage.h
@@ -12,15 +12,21 @@ using namespace std;
 class Storage{
 private:
 	vector<Task> taskList;
+	vector<Task> deadlineTaskList;
+	vector<Task> floatingTaskList;
 
 public:
 	Storage();
 	~Storage();
 
-	void storeTask(string task, string startingTime, string endingTime);
+	//void storeTask(string task, string startingTime, string endingTime);
 	void saveFile(string TEXTFILENAME);
 	vector<Task> getTaskList();
+	vector<Task> getDeadlineTaskList();
+	vector<Task> getFloatingTasklist();
 	void updateTaskList(vector<Task> taskStorage);
+	void updateDeadlineTasklist(vector<Task> deadlineTaskStorage);
+	void updateFloatingTasklist(vector<Task> floatingTaskStorage);
 
 };
 
