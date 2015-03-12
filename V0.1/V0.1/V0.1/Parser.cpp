04e5a21fd547014406f7fb1301df098c50dd6eb5
README <ChiMing710@8-91.priv23.nus.edu.sg>
2015-03-12 16:31:15 +0800

    no message

--- V0.1/V0.1/V0.1/Parser.cpp
+++ V0.1/V0.1/V0.1/Parser.cpp
@@ -30,67 +30,71 @@ bool Parser::determineCommand(string userCommand){
         systemFeedback = true;
         
         size_t task = std::count(detail.begin(), detail.end(), ';');
-        if (task == 3){
+        if (task == 2){
             string taskName;
             string startTime;
             string endTime;
             taskName = getTaskName(detail);
-            cout << "TName is : "<< taskName << "\n";
+            //cout << "TName is : "<< taskName << "\n";
             startTime = getStartDetail2(detail);
-            cout << "start time is : "<< startTime << "\n";
+            //cout << "start time is : "<< startTime << "\n";
             endTime = getEndDetail(detail);
-            cout << "end time is : "<< endTime << "\n";
+            //cout << "end time is : "<< endTime << "\n";
             logic.addTask(taskName, startTime, endTime);
-        }else if (task == 2){
+        }else if (task == 1){
             string taskName;
             string startTime;
             taskName = getTaskName(detail);
-            cout << "TName is : "<< taskName << "\n";
+            //cout << "TName is : "<< taskName << "\n";
             startTime = getStartDetail1(detail);
-            cout << "time is : "<< startTime << "\n";
+            //cout << "time is : "<< startTime << "\n";
             //logic.addTask(taskName, startTime);
         }else{
             string taskName;
             taskName = getTaskName(detail);
-            cout << "TName is : "<< taskName << "\n";
+            //cout << "TName is : "<< taskName << "\n";
             //logic.addTask(taskName);
         }
         
         
-    } /*
+    }
     else if (task == "delete"){
         //command = COMMAND_DELETE;
         systemFeedback = true;
         int index;
-        index = getIndex(userCommand);
-        logic.deleteTask(index);
+        index = getIndex(detail);
+        //cout << "Index is :" << index << "\n";
+        //logic.deleteTask(index);
     }
    
     else if (task == "search"){
         //command = COMMAND_SEARCH;
         systemFeedback = true;
-        
-        string keyWord;
-        cout << USER_PROMPT_KEYWORD;
-        cin >> keyWord;
+        string keyWord = detail;
+        //cout << "keyword is :" << keyWord << "\n";
         logic.searchTask(keyWord);
     }
+    
     else if (task == "edit"){
         //command = COMMAND_EDIT;
         systemFeedback = true;
-        
+        int index;
+        index = getIndex(detail);
+        //cout << "Index is :" << index << "\n";
+        string editDetail = getDetail(detail);
         string taskName;
         string startTime;
         string endTime;
-        taskName = getUserTask(userCommand);
-        startTime = getStartDetail(userCommand);
-        endTime = getEndDetail(userCommand);
-        int index;
-        logic.editTask(index,taskName, startTime, endTime);
-    }*/
+        
+        taskName = getTaskName(editDetail);
+        //cout << "TName is : "<< taskName << "\n";
+        startTime = getStartDetail2(editDetail);
+        //cout << "start time is : "<< startTime << "\n";
+        endTime = getEndDetail(editDetail);
+        //cout << "end time is : "<< endTime << "\n";
+        //logic.editTask(index,taskName, startTime, endTime);
+    }
     else return false;
-    
-    //processCommand(command);
     
     return systemFeedback;
     
@@ -118,17 +122,9 @@ string Parser::getDetail(string input){
 }
 
 int Parser::getIndex(string input){
-    size_t positionA = 0;
-    positionA = input.find_first_of(DELIMITERS);
-    positionA++;
-    string indexs = input.substr(positionA);
-    int index;
-    
-    index = atoi(indexs.c_str());
-    cout << index;
+    int index = atoi(input.c_str());
     return index;
 }
-
 
 string Parser::getTaskName(string input){
     size_t positionA = 0;
@@ -140,9 +136,6 @@ string Parser::getTaskName(string input){
     return task;
 }
 
-
-
-
 string Parser::getStartDetail1(string input){
     size_t positionA = 0;
     size_t positionB = 0;
@@ -151,9 +144,6 @@ string Parser::getStartDetail1(string input){
     positionB = input.find_last_of(DELIMITERS);
     time = input.substr(positionA, positionB - positionA);
     return time;
-    
-    //asdas;32456;
-    //     A     B
 }
 
 string Parser::getStartDetail2(string input){
@@ -162,17 +152,13 @@ string Parser::getStartDetail2(string input){
     size_t positionC = 0;
     string time;
     
-    
     positionA = input.find_first_of(DELIMITERS) + 1;
-    positionC = input.find_last_of(DELIMITERS);
+    positionC = input.size();
     string temp = input.substr(positionA, positionC - positionA);
     positionB = temp.find_first_of(DELIMITERS);
     positionA = 0;
     time = temp.substr(positionA, positionB - positionA);
     return time;
-    
-    //asdas;32456;43546;
-    //     A     B     C
 }
 
 string Parser::getEndDetail(string input){
@@ -181,92 +167,12 @@ string Parser::getEndDetail(string input){
     size_t positionC = 0;
     string time;
     
-    
     positionA = input.find_first_of(DELIMITERS) + 1;
-    positionC = input.find_last_of(DELIMITERS);
+    positionC = input.size();
     string temp = input.substr(positionA, positionC - positionA);
     positionB = temp.find_first_of(DELIMITERS) + 1;
     positionA = temp.find_first_of(DELIMITERS) + 1;
     time = temp.substr(positionB, positionC - positionB);
     return time;
-    
-    //asdas;32456;43546;
-    //     A     B     C
-}
-/*
-bool Parser::determineCommandType(string userCommand){
-    COMMAND_TYPE command;
-    bool systemFeedback = false;
-    if (userCommand == "add"){
-        command = COMMAND_ADD;
-        systemFeedback = true;
-    }
-    else if (userCommand == "delete"){
-        command = COMMAND_DELETE;
-        systemFeedback = true;
-    }
-    else if (userCommand == "search"){
-        command = COMMAND_SEARCH;
-        systemFeedback = true;
-    }
-    else if (userCommand == "edit"){
-        command = COMMAND_EDIT;
-        systemFeedback = true;
-    }
-    else return false;
-    
-    processCommand(command);
-    
-    return systemFeedback;
-    
-}
 
-void Parser::processCommand(COMMAND_TYPE command){
-    switch (command)
-    {
-        case Parser::COMMAND_ADD:{
-            string taskName;
-            string startTime;
-            string endTime;
-            
-            //cout << USER_PROMPT_TASKNAME;
-            cin >> taskName;
-            cout << USER_PROMPT_STARTTIME;
-            cin >> startTime;
-            cout << USER_PROMPT_ENDTIME;
-            cin >> endTime;
-            logic.addTask(taskName, startTime, endTime);
-            break;
-            
-        }case Parser::COMMAND_DELETE:{
-            int index;
-            cout << USER_PROMPT_INDEX;
-            cin >> index;
-            logic.deleteTask(index);
-            break;
-        }case Parser::COMMAND_SEARCH:{
-            string keyWord;
-            cout << USER_PROMPT_KEYWORD;
-            cin >> keyWord;
-            logic.searchTask(keyWord);
-            break;
-        }case Parser::COMMAND_EDIT:{
-            int index;
-            string newTaskName;
-            string newStartTime;
-            string newEndTime;
-            cout << USER_PROMPT_INDEX;
-            cin >> index;
-            cout << USER_PROMPT_NEWTASKNAME;
-            cin >> newTaskName;
-            cout << USER_PROMPT_NEWSTARTTIME;
-            cin >> newStartTime;
-            cout << USER_PROMPT_ENDTIME;
-            cin >> newEndTime;
-            logic.editTask(index, newTaskName, newStartTime, newEndTime);
-            break;
-        }default:
-            break;
-    }
 }
-*/
