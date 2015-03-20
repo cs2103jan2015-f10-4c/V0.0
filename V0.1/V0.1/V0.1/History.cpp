e7185a05c18dcb9e55642eb81fea151ebcba0699
LI HAOYI <jason19920910@gmail.com>
2015-03-20 15:49:32 +0800

    History
    add undo redo method in history class

--- V0.1/V0.1/V0.1/History.cpp
+++ V0.1/V0.1/V0.1/History.cpp
@@ -13,15 +13,52 @@ History::~History(){};
 
 
 void History::saveOperation(vector<Task> taskStorage){
-	undoTaskStorage.push(taskStorage);
+	undoStorageList.push(taskStorage);
 
 };
-void History::undo(vector<Task> taskStorage){
-	if (undoTaskStorage.size() > 2){
-		undoTaskStorage.pop();
+
+bool History::undo(vector<Task> updatedTaskList){
+	bool undoStatue = false;
+	
+	if (!undoStorageList.empty()){
+		redoStorageList.push(undoStorageList.top());
+		
+		if (undoStorageList.size() > 2){
+			undoStorageList.pop();
+
+			undoStatue = true;
+		}
+	
+		updatedTaskList.clear();
+
+		for (int i=0; i<undoStorageList.top().size(); i++){
+			updatedTaskList.push_back(undoStorageList.top()[i]);
+		}
 	}
 
-	for (int i=0; i<undoTaskStorage.top().size(); i++){
-		taskStorage.push_back(undoTaskStorage.top()[i]);
+	if (undoStatue == false){
+		redoStorageList.pop();
 	}
+
+	return undoStatue;
 };
+
+bool History::redo(vector<Task> updatedTaskList){
+	bool redoStatus = false;
+
+	if (!redoStorageList.empty()){
+		updatedTaskList.clear();
+
+		for (int i=0; i<redoStorageList.top().size(); i++){
+			updatedTaskList.push_back(redoStorageList.top()[i]);
+		}
+
+		undoStorageList.push(redoStorageList.top());
+		redoStorageList.pop();
+
+		redoStatus = true;
+	}
+
+	return redoStatus;
+};
+
