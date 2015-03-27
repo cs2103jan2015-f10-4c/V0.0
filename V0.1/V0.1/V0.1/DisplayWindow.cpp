5e6e9ba34e0caa8c5fb4ac28f7e703041d489412
malachite2019 <malachite2019@gmail.com>
2015-03-27 12:16:41 +0800

    display oss

--- V0.1/V0.1/V0.1/DisplayWindow.cpp
+++ V0.1/V0.1/V0.1/DisplayWindow.cpp
@@ -4,7 +4,16 @@ DisplayWindow::DisplayWindow(){}
 DisplayWindow::~DisplayWindow(){}
 
 void DisplayWindow::setContent(vector<Task> tasklist){
-	_content = tasklist;
+   _content = tasklist;
+   ostringstream oss;
+   for (int i = 0; i < tasklist.size(); i++){
+	   oss << i + 1 << '.'
+		   << tasklist[i].taskName
+		   << " from " << tasklist[i].startingTime
+		   << " to " << tasklist[i].endingTime
+		   << " " << tasklist[i].status << endl;
+   }
+   cout << oss.str();
 }
 
 vector<Task> DisplayWindow::getContent(){
