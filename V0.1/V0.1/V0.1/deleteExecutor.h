fa6fa1a0a6fdf10dbf9203db0bd72ea8e88f9c84
jiaaayu <jia_yu_1993@hotmail.com>
2015-04-10 12:59:00 +0800

    refactor (remove useless comments)

--- V0.1/V0.1/V0.1/deleteExecutor.h
+++ V0.1/V0.1/V0.1/deleteExecutor.h
@@ -7,21 +7,14 @@
 #include <iomanip>
 #include <ios>
 #include "Task.h"
-/*#include "Storage.h"
-#include "History.h"
-#include "commonExecutor.h"*/
 
 using namespace std;
 
 class deleteExecutor{
 private:
-   /* Storage storage;
-	History history;
-	Task task;
-	commonExecutor executor;*/
-
+  
 public:
-   // vector<Task> taskList;
+   
     deleteExecutor();
     ~deleteExecutor();
 	void deleteTask(int, vector<Task>&);
