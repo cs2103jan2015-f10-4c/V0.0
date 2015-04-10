b582904103939343f57c9669e19e254edb11a475
jiaaayu <jia_yu_1993@hotmail.com>
2015-04-10 12:25:17 +0800

    refactoring (delete useless comments)

--- V0.1/V0.1/V0.1/addExecutor.cpp
+++ V0.1/V0.1/V0.1/addExecutor.cpp
@@ -8,24 +8,18 @@ addExecutor::addExecutor(void){}
 addExecutor::~addExecutor(void){}
 
 void addExecutor::addTimedTask(string taskTitle, string startTime, string endTime, string taskType, vector<Task>& taskList) {
-    //taskList = storage.getTaskList();
     Task tempStorage;
     tempStorage.taskName = taskTitle;
-    //tempStorage.date = taskDate;
     tempStorage.startingTime = startTime;
     tempStorage.endingTime = endTime;
     tempStorage.status = DEFAULT_TASK_STATUS;
     tempStorage.type = taskType;
     taskList.push_back(tempStorage);
-    //executor.updateStorage(taskList);
-    //testing
-    //storage.saveOperation();
-    //displayAll();
+   
 }
 
 void addExecutor::addDeadlineTask(string taskTitle, string endTime, string taskType, vector<Task>& deadlineList, vector<Task>& taskList) {
-	//deadlineList = storage.getDeadlineTaskList();
-	//taskList = storage.getTaskList();
+	
 	Task tempStorage;
 	tempStorage.taskName = taskTitle;
 	tempStorage.startingTime = EMPTY_STRING;
@@ -34,20 +28,11 @@ void addExecutor::addDeadlineTask(string taskTitle, string endTime, string taskT
 	tempStorage.type = taskType;
 	deadlineList.push_back(tempStorage);
 	taskList.push_back(tempStorage);
-	//updateDeadlineStorage();
-	//executor.updateStorage(taskList);
-	//taskList = storage.getTaskList();
-	//if (taskList.empty()){
-		//cout << "empty";
-	//} empty......
-
-	//storage.saveOperation();
-	//displayAll();
+	
 }
 
 void addExecutor::addFloatingTask(string taskTitle, string taskType, vector<Task>& floatingList, vector<Task>& taskList) {
-	//floatingList = storage.getFloatingTaskList();
-	//taskList = storage.getTaskList();
+	
 	Task tempStorage;
 	tempStorage.taskName = taskTitle;
 	tempStorage.startingTime = EMPTY_STRING;
@@ -56,9 +41,5 @@ void addExecutor::addFloatingTask(string taskTitle, string taskType, vector<Task
 	tempStorage.status = DEFAULT_TASK_STATUS;
 	floatingList.push_back(tempStorage);
 	taskList.push_back(tempStorage);
-	//updateFloatingStorage()t
-	//executor.updateStorage(taskList);
-	//storage.saveOperation();
-	//displayFloating();
-	//displayAll();
+	
 }
