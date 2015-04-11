790a0d323707623098da13c5b74aae8251685ac8
LI HAOYI <jason19920910@gmail.com>
2015-04-11 21:55:27 +0800

    Revert "Revert "remove done task for default display""
    This reverts commit 3dcd18aad24e63ee28e929745fc96a0f24d4040a.

--- V0.1/V0.1/V0.1/Logic.cpp
+++ V0.1/V0.1/V0.1/Logic.cpp
@@ -88,9 +88,7 @@ void Logic::addTask(){
 		add.addTimedTask(taskName, startTime, endTime, taskType, taskList);
 	} else if (taskType == "deadline") {
 		string taskName = parse.getTaskName();
-		//cout << taskName; passed
 		string endTime = parse.getEndTime();
-		//cout << endTime; debugged
 		add.addDeadlineTask(taskName, endTime, taskType, deadlineList,taskList);
 	} else if (taskType == "floating") {
 		string taskName = parse.getTaskName();
@@ -98,7 +96,7 @@ void Logic::addTask(){
 	}
 	history.saveOperation(taskList);
 	response.addResponse(isAddedValidTime, isAddedCorrectFormat);
-	disp.setDefaultDisplay(taskList);
+	removeDoneTaskForDisplayDefault();
 	storage.saveFile(taskList);
 	}else if (!isAddedValidTime) {
 		response.addResponse(isAddedValidTime, true);
@@ -115,7 +113,7 @@ void Logic::deleteTask() {
 	    deleteATask.deleteTask(index, taskList);
 	    history.saveOperation(taskList);
 		response.deleteResponse(isCorrectIndex, index);
-	    disp.setDefaultDisplay(taskList);
+	    removeDoneTaskForDisplayDefault();
 	    storage.saveFile(taskList);
 	} else if (!checkIndex(index)) {
 		response.deleteResponse(isCorrectIndex, index);
@@ -134,7 +132,7 @@ void Logic::editTask(){
 	    edit.editTask(index, newTaskName, newStartTime, newEndTime, newTaskType, taskList);
 	    history.saveOperation(taskList);
 		response.editResponse(isCorrectIndex, index);
-	    disp.setDefaultDisplay(taskList);
+	    removeDoneTaskForDisplayDefault();
 	    storage.saveFile(taskList);
 	} else {
 		response.editResponse(isCorrectIndex, index);
@@ -279,4 +277,13 @@ void Logic::clearTaskList() {
 
 void Logic::exitProgram() {
 	exit(EXIT_SUCCESS);
+}
+
+void Logic::removeDoneTaskForDisplayDefault() {
+	for (int i = 0; i < taskList.size(); i++) {
+		if (taskList[i].status != "done") {
+			taskListWithoutDone.push_back(taskList[i]);
+		}
+	}
+	disp.setDefaultDisplay(taskListWithoutDone);
 }
--- V0.1/V0.1/V0.1/Logic.h
+++ V0.1/V0.1/V0.1/Logic.h
@@ -47,6 +47,7 @@ public:
     vector<Task> taskList;
 	vector<Task> deadlineList;
     vector<Task> floatingList;
+	vector<Task> taskListWithoutDone;
 
 	enum COMMAND_TYPE {
 
@@ -67,6 +68,7 @@ public:
 	void clearTaskList();
 	void exitProgram();
 	void showUserInvalidResponse();
+	void removeDoneTaskForDisplayDefault();
 
 	string tellGUI();
 	string tellGUIResponse();
