00878dab23ecc3b13a2895092d44924a94be7344
jiaaayu <jia_yu_1993@hotmail.com>
2015-04-13 19:30:35 +0800

    coding standard

--- V0.1/V0.1/V0.1/editExecutor.cpp
+++ V0.1/V0.1/V0.1/editExecutor.cpp
@@ -1,12 +1,12 @@
 #include "editExecutor.h"
 
-string editExecutor::DEFAULT_TASK_STATUS = "ongoing";
+string EditExecutor::DEFAULT_TASK_STATUS = "ongoing";
 
-editExecutor::editExecutor(void){}
+EditExecutor::EditExecutor(void) {}
 
-editExecutor::~editExecutor(void){}
+EditExecutor::~EditExecutor(void) {}
 
-void editExecutor::editTask(int index, string newTaskName, string newStartTime, string newEndTime, string newTaskType, vector<Task>& taskList){
+void EditExecutor::editTask(int index, string newTaskName, string newStartTime, string newEndTime, string newTaskType, vector<Task>& taskList) {
 	
 	taskList[index - 1].taskName = newTaskName;
 	taskList[index - 1].startingTime = newStartTime;
--- V0.1/V0.1/V0.1/editExecutor.h
+++ V0.1/V0.1/V0.1/editExecutor.h
@@ -12,15 +12,19 @@
 
 using namespace std;
 
-class editExecutor{
+//EditExecutor class is used to perform actions related to edit
+//This class has function to take in index of a task and relevant information has been modified by the user
+//This class takes in the taskList from logic and makes modification directly in the list
+
+class EditExecutor{
 private:
    
 	static string DEFAULT_TASK_STATUS;
 
 public:
   
-    editExecutor();
-    ~editExecutor();
+    EditExecutor();
+    ~EditExecutor();
     void editTask(int, string, string, string, string, vector<Task>&);
 };
 
